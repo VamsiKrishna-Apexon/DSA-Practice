18503e2d3eda33a65083038ca82ed78167018149
Vamsi Krishna Maddina <Vamsi.Maddina@apexon.com>
2025-07-15 16:13:53 +0530

    Adding DSA Problems - Fibonocci

---
+++ FibonocciDP.java
@@ -0,0 +1,23 @@
+public class FibonocciDP {
+
+    public static int fibonocci(int n) {
+        if (n<=1)
+            return n;
+        // 0 1 1 2 3 5 8
+       // 0 1, 1 1, 1 2, 2 3
+        int dp[] = new int[]{0,1};
+        int res=0;
+        for(int i=2;i<=n;i++){
+            int temp = dp[1];
+            dp[1] = dp[0] + dp[1];
+            dp[0] = temp;
+            //System.out.println(dp[0] + " " + dp[1]);
+        }
+        return dp[1];
+    }
+
+    // Test the function
+    public static void main(String[] args) {
+        System.out.println(fibonocci(7));  // Output: 8
+    }
+}
